59314553d8eb571ba5123367215a6d9e4d716a1f
Niks Bugnēvics <70239899+flaffii@users.noreply.github.com>
2023-02-20 00:35:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,27 +13,24 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket
             opening_brackets_stack.append(Bracket(next, i))
 
         if next in ")]}":
-            # Process closing bracket, write your code here
-                        if not opening_brackets_stack:
-                return i + 1
-            top = opening_brackets_stack.pop()
-            if not are_matching(top.char, next):
-                return i + 1
+            if not opening_brackets_stack:
+                return i+1
+            top_bracket = opening_brackets_stack.pop()
+            if not are_matching(top_bracket.char, next):
+                return i+1
     if opening_brackets_stack:
         return opening_brackets_stack[0].position + 1
     return "Success"
 
-
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer
     print(mismatch)
 
 
 if __name__ == "__main__":
     main()
+
